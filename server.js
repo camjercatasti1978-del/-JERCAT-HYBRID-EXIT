0bd13d60a7d8f19a0367016124af9b88f33d9f33
camjercatasti1978-del <camjercatasti1978@gmail.com>
2025-12-05 20:46:09 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Bot running on port ${PORT}`);
+});
